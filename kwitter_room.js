a7ffe414892bec67f9201b69b7e4e289c8658699
SeChVeAha <140607001+SeChVeAha@users.noreply.github.com>
2023-12-27 18:20:27 +0000

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+//ADD YOUR FIREBASE LINKS HERE
+var firebaseConfig = {
+    apiKey: "AIzaSyAwlwa8GZk3aApGUjr2kVb4fvfFmQUjm9o",
+    authDomain: "kwitter-b3039.firebaseapp.com",
+    databaseURL: "https://kwitter-b3039-default-rtdb.firebaseio.com",
+    projectId: "kwitter-b3039",
+    storageBucket: "kwitter-b3039.appspot.com",
+    messagingSenderId: "240299408699",
+    appId: "1:240299408699:web:8db4eedc3ace76007a8b1e"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
